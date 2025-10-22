8bf247f0c7e0d02743b7d7bb900f9728457838a5
Ronny Freites <ronnyangelo.freites@gmail.com>
2025-10-21 21:38:54 -0600

    fix: remove Chakra UI dependencies from page, hide old sections for Phase 3 migration

--- nextjs-project/src/app/[locale]/page.tsx
+++ nextjs-project/src/app/[locale]/page.tsx
@@ -1,26 +1,21 @@
 import Navigation from '@/components/Navigation'
 import HeroSection from '@/components/sections/HeroSection'
 import AboutSection from '@/components/sections/AboutSection'
-import ExperienceSection from '@/components/ExperienceSection'
-import SkillsSection from '@/components/SkillsSection'
-import ProjectsSection from '@/components/ProjectsSection'
-import DownloadCVSection from '@/components/DownloadCVSection'
-import ContactSection from '@/components/ContactSection'
 import Footer from '@/components/sections/Footer/Footer'
-import { Box } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Box overflowX="hidden">
+    <main className="overflow-x-hidden">
       <Navigation />
       <HeroSection />
       <AboutSection />
-      <ExperienceSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <DownloadCVSection />
-      <ContactSection />
+      {/* Existing Chakra UI sections temporarily hidden - will be migrated in Phase 3 */}
+      {/* <ExperienceSection /> */}
+      {/* <SkillsSection /> */}
+      {/* <ProjectsSection /> */}
+      {/* <DownloadCVSection /> */}
+      {/* <ContactSection /> */}
       <Footer />
-    </Box>
+    </main>
   );
 }
